037d230fd9eb3e5a66bda33385ed2f4e66a15da1
jagerf <fores.dd@gmail.com>
2016-05-17 11:55:51 +0200

    Throttle min en stabilize
    No poner motores en SPIN_WHEN_ARMED cuanto del throttle esta en minimo.

--- ArduCopter/control_stabilize.cpp
+++ ArduCopter/control_stabilize.cpp
@@ -28,7 +28,7 @@ void Copter::stabilize_run()
     float pilot_throttle_scaled;
 
     // if not armed set throttle to zero and exit immediately
-    if (!motors.armed() || ap.throttle_zero || !motors.get_interlock()) {
+    if (!motors.armed() || !motors.get_interlock()) {
         motors.set_desired_spool_state(AP_Motors::DESIRED_SPIN_WHEN_ARMED);
         attitude_control.set_throttle_out_unstabilized(0,true,g.throttle_filt);
         return;
