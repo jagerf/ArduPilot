e7e65bfeaa4ec9ac2ad44a34af771532f0d1ee90
jagerf <fores.dd@gmail.com>
2016-05-18 10:00:20 +0200

    Edit control_drop
    erag

--- ArduCopter/control_drop.cpp
+++ ArduCopter/control_drop.cpp
@@ -1,18 +1,19 @@
-//
-// Created by David Forés Diaz on 17/5/16.
-//
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 #include "Copter.h"
 
 
 
 // drop_init - initialise land controller
-bool Copter::drop_init(bool ignore_checks) {
+bool Copter::drop_init(bool ignore_checks)
+{
 
+
+    motors.set_desired_spool_state(AP_Motors::DESIRED_SPIN_WHEN_ARMED);
     // initialize vertical speeds and leash lengths
     pos_control.set_speed_z(-1200, 500);
 
-    
+
     //pos_control.set_accel_z(wp_nav.get_accel_z());
 
 
@@ -29,6 +30,12 @@ void Copter::drop_run()
 
     float target_roll = 0.0f, target_pitch = 0.0f;
     float target_yaw_rate = 0;
+
+    // set motors to full range
+    motors.set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
+
+    // call attitude controller
+    attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw_smooth(target_roll, target_pitch, target_yaw_rate, get_smoothing_gain());
 
 
     pos_control.accel_to_throttle(-981);
