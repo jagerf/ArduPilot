3fe06f6e8fffd86da0f8ec0b0f993ff028591414
jagerf <fores.dd@gmail.com>
2016-05-30 12:23:23 +0200

    mod drop
    mod drop

--- ArduCopter/control_drop.cpp
+++ ArduCopter/control_drop.cpp
@@ -9,6 +9,7 @@ bool Copter::drop_init(bool ignore_checks)
 {
 
     drop_time_start = millis();
+    g.drop_acc = 0;
     chute = true;
     return true;
 
@@ -24,7 +25,7 @@ void Copter::drop_run()
     int diff = millis()-drop_time_start;
 
 
-    if(diff < g.drop_hold_time) {
+    if(diff < g.drop_time) {
 
             // set motors to full range
             motors.set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
@@ -33,38 +34,29 @@ void Copter::drop_run()
             attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw_smooth(target_roll, target_pitch,
                                                                                 target_yaw_rate, get_smoothing_gain());
 
-            pos_control.set_alt_target_from_climb_rate(cmb_rate, G_Dt, true);
-            pos_control.update_z_controller();
-
-    }else{
-        if(diff < (g.drop_hold_time + g.drop_time)) {
-
-            // set motors to full range
-            motors.set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
-
-            // call attitude controller
-            attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw_smooth(target_roll, target_pitch,
-                                                                                target_yaw_rate,
-                                                                                get_smoothing_gain());
-
-
+            if(g.drop_acc > - 975) {
+                g.drop_acc = - 330 * (diff/1000);
+            }
+            else {
+                g.drop_acc = - 981;
+            }
             pos_control.accel_to_throttle(g.drop_acc);
 
-        }else{
+    }else{
 
-            if(chute) {
-                // send message to gcs and dataflash
-                gcs_send_text(MAV_SEVERITY_INFO, "Parachute: Released");
-                Log_Write_Event(DATA_PARACHUTE_RELEASED);
+        if(chute) {
+            // send message to gcs and dataflash
+            gcs_send_text(MAV_SEVERITY_INFO, "Parachute: Released");
+            Log_Write_Event(DATA_PARACHUTE_RELEASED);
 
-                // disarm motors
-                init_disarm_motors();
+            // disarm motors
+            init_disarm_motors();
 
-                // release parachute
-                parachute.release();
-                chute = false;
-            }
+            // release parachute
+            parachute.release();
+            chute = false;
         }
+    }
     }
 
 
